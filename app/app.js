23c44b5f9e9c046402d169c04e98bb330e0013b0
Luis Blanco 路易斯.布兰科 <luis_blanco@volusion.com>
2015-01-07 19:21:28 -0600

    Site working with fake data

---
+++ app/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by luis_blanco on 1/7/2015.
+ */
+(function () {
+    "use strict";
+
+    var app = angular.module('productManagement', []);
+}());
---
+++ app/products/productListCtrl.js
@@ -0,0 +1,41 @@
+/**
+ * Created by luis_blanco on 1/7/2015.
+ */
+(function () {
+    "use strict";
+    angular
+        .module("productManagement")
+        .controller("ProductListCrtl", ProductListCrtl);
+
+    function ProductListCrtl() {
+        // when using the controller as syntax, the model is defined in this variable.
+        var vm = this;
+
+        vm.products = [
+            {
+                "productId": 1,
+                "productName": "Leaf Rake",
+                "productCode": "GDN-0011",
+                "releaseDate": "March 19, 2009",
+                "description": "Leaf rake with 48-inch handle.",
+                "cost": 9.00,
+                "price": 19.95,
+                "category": "garden",
+                "tags": ["leaf", "tool"],
+                "imageUrl": "https://openclipart.org/image/800px/svg_to_png/183161/worms-x-012c8714.png"
+            },
+            {
+                "productId": 2,
+                "productName": "Hammer",
+                "productCode": "TBX-0048",
+                "releaseDate": "May 21, 2013",
+                "description": "Curved claw steel hammer.",
+                "cost": 1.00,
+                "price": 8.99,
+                "category": "toolbox",
+                "tags": ["tool"],
+                "imageUrl": "https://openclipart.org/image/800px/svg_to_png/2507/Machovka-Pipe-wrench.png"
+            }
+        ];
+    }
+}());
